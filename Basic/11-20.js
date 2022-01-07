48e121dec9be1b64a335ef10a0f8b045283b6f0b
p.lukyanov <p.lukyanov@qa-academy.by>
2022-01-07 03:58:49 +0300

    complete 11-20

---
+++ Basic/11-20.js
@@ -0,0 +1,38 @@
+//11
+const sum = 10 + 10;
+
+//12
+const difference = 45 - 33;
+
+//13
+const product = 8 * 10;
+
+//14
+const quotient = 66 / 33;
+
+//15
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+
+//16
+let myVar = 11;
+
+// Only change code below this line
+myVar--;
+
+//17
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal = 5.7;
+
+//18
+const product = 2.0 * 2.5;
+
+//19
+const quotient = 4.4 / 2.0; // Change this line
+
+//20
+const remainder = 11 % 3;
