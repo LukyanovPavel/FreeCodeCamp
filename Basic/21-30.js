f181a36375e50b7ff2b6fa96c44af2c78adbfa68
p.lukyanov <p.lukyanov@qa-academy.by>
2022-01-08 05:05:00 +0300

    complete 21-30

---
+++ Basic/21-30.js
@@ -0,0 +1,60 @@
+//21
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//22
+let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;
+
+//23
+let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;
+
+//24
+let a = 48;
+let b = 108;
+let c = 33;
+
+// Only change code below this line
+a /= 12;
+b /= 4;
+c /= 11;
+
+//25
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Change this line
+
+//26
+const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+//27
+const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; // Change this line
+
+//28
+const myStr = "This is the start. " + "This is the end."; // Change this line
+
+//29
+let myStr = 'This is the first sentence.';
+myStr += ' This is the second sentence.'
+
+//30
+// Only change code below this line
+const myName = "Pavel";
+const myStr = "My name is" + myName +"and I am well!";
