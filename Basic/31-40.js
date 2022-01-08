17b313db5229120eba0a5d893ae0c1039cf831ec
p.lukyanov <p.lukyanov@qa-academy.by>
2022-01-08 05:42:05 +0300

    complete 31-40

---
+++ Basic/31-40.js
@@ -0,0 +1,68 @@
+//31
+// Change code below this line
+const someAdjective = "qwer";
+let myStr = "Learning to code is ";
+myStr += someAdjective;
+
+//32
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+
+//33
+// Setup
+let firstLetterOfLastName = "";
+const lastName = "Lovelace";
+
+// Only change code below this line
+firstLetterOfLastName = lastName[0]; // Change this line
+
+//34
+// Setup
+let myStr = "Jello World";
+
+// Only change code below this line
+myStr = "Hello World"; // Change this line
+// Only change code above this line
+
+//35
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+//36
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length - 1]; // Change this line
+
+//37
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length - 2]; // Change this line
+
+//38
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+
+//39
+// Only change code below this line
+const myArray = ['string', 5];
+
+//40
+// Only change code below this line
+const myArray = [[]];
